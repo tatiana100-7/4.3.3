74c4c946ae32cb77fd3f5ee6c69dcddb7022549a
Tatiana <tanya.yashina.08@mail.ru>
2023-06-02 12:55:39 +0300

    загрузили сайт

---
+++ data.js
@@ -0,0 +1,20 @@
+export const context = {
+  languages: [
+    {
+      name: "HTML",
+      modern: true,
+    },
+    {
+      name: "CSS",
+      modern: true,
+    },
+    {
+      name: "JavaScript",
+      modern: true,
+    },
+    {
+      name: "COBOL",
+      modern: false,
+    },
+  ],
+};
